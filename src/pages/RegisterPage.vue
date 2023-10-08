89e08f9ffa8fb18f417be7ad371a92bf453d7715
Gustav OL <70300539+Gustav-Dev17@users.noreply.github.com>
2023-10-08 19:23:40 -0400

    ✨ Add register page features

--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -1,110 +1,157 @@
 <template>
   <q-page class="column items-center justify-center">
 
-      <h1 class="signin-card-title">Criar conta</h1>
+    <h1 class="signin-card-title">Criar conta</h1>
 
-      <div class="signin-card">
-        <q-form class="signin-form" @submit="onSubmit">
+    <div class="signin-card">
+      <q-form class="signin-form" @submit="onSubmit">
 
-          <div class="signin-form-group">
-            <label class="signin-form-label" for="nome">Nome de organizador <span class="is-required">*</span></label>
-            <input type="text" id="nome" class="signin-form-input" placeholder="Digite seu nome">
-          </div>
+        <div class="signin-form-group">
+          <label class="signin-form-label" for="input_name">Nome de organizador <span class="is-required">*</span></label>
+          <input v-model="form.name" type="text" id="input_name" class="signin-form-input"
+            placeholder="Digite o nome do organizador" required />
+        </div>
 
-          <div class="signin-form-group">
-            <label class="signin-form-label" for="id_document_number">Número do documento <span
-                class="is-required">*</span></label>
-            <input class="signin-form-input" type="text" id="id_document_number" name="id_document_number"
-              placeholder="Digite o número de CPF ou CNPJ">
-          </div>
+        <div class="signin-form-group">
+          <label class="signin-form-label" for="input_id_document_number">Número do documento <span
+              class="is-required">*</span></label>
+          <input v-maska:[docMask] v-model="form.id_document_number" class="signin-form-input" type="text"
+            id="input_id_document_number" name="id_document_number" placeholder="Digite o número de CPF ou CNPJ"
+            required />
+        </div>
 
-          <div class="signin-form-group">
-            <label class="signin-form-label" for="id_document_number">Contato (WhatsApp) <span
-                class="is-required">*</span></label>
-            <input class="signin-form-input" type="tel" id="phone" name="phone" placeholder="Digite seu contato">
-          </div>
+        <div class="signin-form-group">
+          <label class="signin-form-label" for="input_phone">Contato (WhatsApp) <span class="is-required">*</span></label>
+          <input v-model="form.phone" v-maska data-maska="(##) #####-####" class="signin-form-input" type="tel"
+            id="input_phone" name="phone" placeholder="Digite seu contato" required />
+        </div>
 
-          <div class="signin-form-group">
-            <label class="signin-form-label" for="id_document_number">URL do Instagram</label>
-            <input class="signin-form-input" type="text" id="id_document_number" name="id_document_number"
-              placeholder="Insira a URL do perfil do Instagram">
-          </div>
+        <div class="signin-form-group">
+          <label class="signin-form-label" for="id_url_instagram">URL do Instagram</label>
+          <input v-model="form.instagram_url" class="signin-form-input" type="text" id="id_url_instagram"
+            name="id_document_number" placeholder="Insira a URL do perfil do Instagram" />
+        </div>
 
-          <div class="signin-form-group">
-            <label class="signin-form-label" for="email">E-mail <span class="is-required">*</span></label>
-            <input type="email" id="email" class="signin-form-input" placeholder="Digite seu email">
-          </div>
+        <div class="signin-form-group">
+          <label class="signin-form-label" for="input_email">E-mail <span class="is-required">*</span></label>
+          <input v-model="form.email" type="email" id="input_email" class="signin-form-input"
+            placeholder="Digite seu email" required />
+        </div>
 
-          <div class="signin-form-group">
-            <label class="signin-form-label" for="senha">Senha <span class="is-required">*</span></label>
-            <input type="password" id="senha" class="signin-form-input" placeholder="Digite sua senha">
-          </div>
+        <div class="signin-form-group">
+          <label class="signin-form-label" for="input_senha">Senha <span class="is-required">*</span></label>
+          <input v-model="form.password" type="password" id="input_senha" class="signin-form-input"
+            placeholder="Digite sua senha" min="8" required />
+        </div>
 
-          <div class="signin-form-button-container">
-            <button class="signin-form-button" label="Submit" type="submit">Registrar-me</button>
-          </div>
+        <div class="signin-form-group">
+          <label class="signin-form-label" for="confirmação_senha">Confirmação de senha <span
+              class="is-required">*</span></label>
+          <input v-model="confPassword" type="confPassword" id="confirmação_senha" class="signin-form-input"
+            placeholder="Digite a confirmação de senha" min="8" required />
+        </div>
 
-          <router-link class="generic-link" to="/login">Já tenho uma conta</router-link>
 
+        <div class="signin-form-box">
+          <input v-model="accept" type="checkbox" id="aceitoTermos" class="signin-form-checkbox" />
+          <label class="signin-form-label" for="aceitoTermos">Aceito os termos e condições</label>
+        </div>
 
-        </q-form>
-      </div>
+        <div class="signin-form-button-container">
+          <button class="signin-form-button" label="Submit" type="submit" :disabled="loading">
+            <span v-if="loading" class="loading-spinner"></span>
+            {{ loading ? 'Aguarde...' : 'Registrar-me' }}
+          </button>
+        </div>
+
+        <router-link class="generic-link" to="/login">Já tenho uma conta</router-link>
+
+      </q-form>
+    </div>
 
 
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { ref } from 'vue'
+import { api } from 'boot/axios';
+import { useQuasar } from 'quasar';
+import { useRouter } from 'vue-router';
+import { defineComponent, Ref } from 'vue';
+import { reactive, ref, watch } from 'vue';
+import { FormRegister } from '../types/form.types';
 
 export default defineComponent({
   name: 'RegisterPage',
 
   setup() {
+    const router = useRouter();
+    const $q = useQuasar();
+    const loading = ref(false);
+    const docMask = reactive({ mask: '###.###.###-##', eager: true });
+    const confPassword = ref('');
+    const accept = ref(false);
 
-    const accept = ref(false)
-    const name = ref(null);
-    const email = ref(null);
-    const password = ref(null);
-    const id_document = ref(null);
-    const id_document_number = ref(null);
-    const phone = ref(null);
-    const instagram_url = ref(null);
+    const form: Ref<FormRegister> = ref({
+      name: '',
+      email: '',
+      password: '',
+      id_document: '',
+      id_document_number: '',
+      phone: '',
+      instagram_url: ''
+    });
+
+    watch(() => form.value.id_document_number, (newValue) => {
+      const length = newValue?.length ?? 0;
+      docMask.mask = length > 14 ? '##.###.###/####-##' : '###.###.###-##';
+      form.value.id_document = length > 14 ? 'CNPJ' : 'CPF';
+    });
 
     const onSubmit = () => {
-      // Aqui você pode acessar os valores dos campos
-      console.log('Nome:', name.value);
-      console.log('E-mail:', email.value);
-      console.log('Senha:', password.value);
-      console.log('Tipo de Documento:', id_document.value);
-      console.log('Número do Documento:', id_document_number.value);
-      console.log('Telefone:', phone.value);
-      console.log('URL do Instagram:', instagram_url.value);
 
-      // Você pode adicionar sua lógica de validação aqui
-
-      // Exemplo: Verificar se o campo de aceitação está marcado
-      if (!accept.value) {
-        console.log('Você precisa aceitar os termos e condições.');
+      if ((form.value.id_document_number.length > 14 && form.value.id_document_number.length < 18) || (form.value.id_document_number.length < 14)) {
+        $q.notify({ type: 'negative', message: 'O número de documento está incompleto!' });
         return;
       }
 
-      // Se tudo estiver válido, você pode enviar o formulário ou realizar outras ações
-      console.log('Formulário enviado com sucesso!');
+      if (!accept.value) {
+        $q.notify({ type: 'negative', message: 'Você precisa aceitar os termos e condições!' });
+        return;
+      }
+
+      if (form.value.password != confPassword.value) {
+        $q.notify({ type: 'negative', message: 'Senha e confirmação de senha não conferem!' });
+        return;
+      }
+
+      loading.value = true;
+
+      api.post('/createOrganiser', form.value)
+        .then(() => {
+          $q.notify({ type: 'positive', message: 'Conta criada com sucesso! Redirecionando...' });
+
+          setTimeout(() => {
+            router.push({ path: '/login' });
+          }, 2000);
+
+        })
+        .catch((e) => {
+          $q.notify({ type: 'negative', message: e.response.data.message || e.response.data.error[0] });
+        })
+        .finally(() => {
+          loading.value = false;
+        })
+
     };
 
     return {
-      name,
-      email,
-      password,
-      id_document,
-      id_document_number,
-      phone,
-      instagram_url,
+      form,
+      confPassword,
       accept,
-
-      onSubmit
+      loading,
+      onSubmit,
+      docMask
     };
 
   }
---
+++ src/types/form.types.ts
@@ -0,0 +1,9 @@
+export interface FormRegister {
+  name: string;
+  email: string;
+  password: string;
+  id_document: string;
+  id_document_number: string;
+  phone: string;
+  instagram_url?: string;
+}
